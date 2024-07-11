c652e884cd3349e86334321246891e36ee67942e
enyanforh <nyanforhen@gmail.com>
2024-07-11 16:50:35 -0700

    User Homepage Done

--- movitas/src/app/store.js
+++ movitas/src/app/store.js
@@ -1,11 +1,18 @@
 import { configureStore} from "@reduxjs/toolkit";
 import userReducer from "../feautures/users/userSlice";
+import movieReducer from "../feautures/movie/movieSlice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import db from "../firebase";
+import { setMovies } from "../feautures/movie/movieSlice";
+import { selectUserName } from "../feautures/users/userSlice";
+
 
 
 export default configureStore({
   reducer: {
     user: userReducer,
-    // movie: movieReducer,
+    movie: movieReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
--- movitas/src/components/Home.js
+++ movitas/src/components/Home.js
@@ -1,20 +1,68 @@
 import styled from "styled-components";
 import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
-// import Recommends from "./Recommends";
-// import NewDisney from "./NewDisney";
-// import Originals from "./orginals";
-// import Trending from "./Trending";
+import Recommends from "./Recommends";
+import NewDisney from "./NewDisney";
+import Originals from "./orginals";
+import Trending from "./Trending";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import db from "../firebase";
+import { useSelector } from "react-redux";
+import { setMovies } from "../feautures/movie/movieSlice";
+import { selectUserName } from "../feautures/users/userSlice";
 
 const Home = (props) => {
+
+    const dispatch = useDispatch();
+    const userName = useSelector(selectUserName);
+    let recommends = [];
+    let newDisneys = [];
+    let originals = [];
+    let trending = [];
+
+    useEffect(() => {
+        db.collection('movies').onSnapshot((snapshot) => {
+            snapshot.docs.map((doc) => {
+                console.log(recommends);
+                switch(doc.data().type) {
+                    case 'recommend':
+                        recommends = [...recommends, {id: doc.id, ...doc.data()}];
+                        break;
+                    
+                    case 'new' :
+                        newDisneys = [...newDisneys, {id: doc.id, ...doc.data()}];
+                        break;
+                    
+                    case 'original' :
+                        originals = [...originals, {id: doc.id, ...doc.data()}];
+                        break;
+                    
+                    case 'trending':
+                        trending= [...trending, {id: doc.id, ...doc.data()}]
+                        break;      
+                }
+            });
+        
+
+        dispatch(
+            setMovies({
+                recommend: recommends,
+                newDisney: newDisneys,
+                original: originals,
+                trending: trending,
+            })
+        );
+    });
+    }, [userName]);
     return(
     <Container>
         <ImgSlider/>
         <Viewers/>
-        {/* <Recommends/> */}
-        {/* <NewDisney/> */}
-        {/* <Originals/> */}
-        {/* <Trending/> */}
+        <Recommends/>
+        <NewDisney/>
+        <Originals/>
+        <Trending/>
     </Container>
     )
 }
